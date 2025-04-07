da1bf4fa235653f98336fbf7c6628b17906fed91
NShevtsov <neketobro@gamil.com>
2025-04-07 12:05:06 +0300

    config file for production

--- frontend/my-social-network-app/vite.config.js
+++ frontend/my-social-network-app/vite.config.js
@@ -1,10 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import dotenv from 'dotenv';
+dotenv.config();
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: process.env.PORT || 4000,
   },
-})
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'esbuild',
+    target: 'esnext',
+    cssCodeSplit: true,
+    assetsInlineLimit: 4096,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+        },
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+});
